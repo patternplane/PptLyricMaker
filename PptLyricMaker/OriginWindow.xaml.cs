d317204b365d279ac99a5e245396f841c9c72e36
patternplane <bss011202@naver.com>
2022-03-31 15:50:04 +0900

    [XAML] 기본 GUI 완성

--- PptLyricMaker/OriginWindow.xaml.cs
+++ PptLyricMaker/OriginWindow.xaml.cs
@@ -10,28 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PptLyricMaker
 {
-    class ModifyButtonVisibility
-    {
-        public bool showButton { get; set; }
-    }
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// Window1.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OriginWindow : Window
     {
-        
-        public MainWindow()
+        public OriginWindow()
         {
             InitializeComponent();
-
-            ModifyButtonVisibility ModifyButtonVisible = new ModifyButtonVisibility() {showButton = false};
-            ModifyButtonGrid.DataContext = ModifyButtonVisible;
-
         }
     }
 }
