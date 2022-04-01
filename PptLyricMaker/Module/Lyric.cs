251725d9b1e185701d9e163c37c36543edf0de6e
patternplane <bss011202@naver.com>
2022-04-01 22:03:01 +0900

    [FEAT] 가사 처리 Lyric 클래스 도입
    파일에서 불러오기 / 파일로 저장하기 기능이 구현됨

---
+++ PptLyricMaker/Module/Lyric.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Windows;
+
+namespace PptLyricMaker.Module
+{
+    /// <summary>
+    /// 가사 관련 작업을 담당합니다.
+    /// </summary>
+    public class Lyric
+    {
+        private List<SingleLyric> lyricArray;
+
+        public Lyric()
+        {
+            lyricArray = LyricFile.getLyricInfo();
+
+            if (lyricArray == null)
+                MessageBox.Show("가사 불러오기 실패!\n프로그램을 종료 후 다시 시작해주세요.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public void SaveAll()
+        {
+            try
+            {
+                LyricFile.SaveAll(lyricArray);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("가사 저장 실패!\n오류 : " + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// ■ 테스트용 함수
+        /// lyric 생성 결과를 출력해주는 함수
+        /// </summary>
+        /// <returns>
+        /// lyric 가사를 String[] 형식으로 반환합니다.<br/>
+        /// 결과 문자열은 [제목 + 줄바꿈 + 본문] 꼴로 출력됩니다.
+        /// </returns>
+        public String[] testOutput()
+        {
+            int cnt = lyricArray.Count;
+            String[] lyrics = new String[cnt];
+            for (int i = 0; i < cnt;i++)
+                lyrics[i] = (lyricArray[i].title.Length == 0 ? "[null]" : lyricArray[i].title) + (char)0xd + (char)0xa + (lyricArray[i].content.Length == 0 ? "[null]" : lyricArray[i].content);
+
+            return lyrics;
+        }
+    }
+
+    /// <summary>
+    /// 곡의 한 단위를 나타냅니다.
+    /// </summary>
+    class SingleLyric
+    {
+        public String title { get; set; }
+        public String content { get; set; }
+    }
+
+    /// <summary>
+    /// Lyrics 저장 파일의 입출력에 관여합니다.
+    /// </summary>
+    class LyricFile
+    {
+        const char SEPARATOR = '∂';
+        const char CR = (char)0xd;
+        const string DATA_PATH = ".\\data";
+        const string LYRIC_PATH = ".\\data\\Lyrics";
+
+        /// <summary>
+        /// 파일로부터 전체 가사를 읽어 리스트로 변환합니다.
+        /// </summary>
+        /// <returns>
+        /// 가사 리스트.<br/>
+        /// 읽기에 실패하면 null을 반환합니다.
+        /// </returns>
+        public static List<SingleLyric> getLyricInfo()
+        {
+            List<SingleLyric> lyricArray;
+
+            try
+            {
+                // 파일 존재 확인
+                FileInfo fi = new FileInfo(LYRIC_PATH);
+                if (!fi.Exists)
+                {
+                    DirectoryInfo di = new DirectoryInfo(DATA_PATH);
+                    if (!di.Exists)
+                    {
+                        di.Create();
+                    }
+
+                    lyricArray = new List<SingleLyric>(0);
+                }
+                else
+                {
+                    lyricArray = new List<SingleLyric>(50);
+
+                    // 파일의 내용을 버퍼 단위로 읽어옴
+                    StreamReader file = new StreamReader(LYRIC_PATH);
+                    int BUF_LEN = 4096;
+                    char[] buffer = new char[BUF_LEN];
+                    int readLen;
+
+                    StringBuilder content = new StringBuilder("");
+                    int startPos; // 버퍼에서 읽어올 첫 인덱스
+                    int endPos; // 버퍼에서 읽어올 마지막 인덱스 + 1
+
+                    int titleEndPosition;
+
+                    while ((readLen = file.Read(buffer, 0, BUF_LEN)) > 0)
+                    {
+                        // 버퍼를 읽어가면서 SEPARATOR 문자로 구분하여 곡을 저장한다.
+                        startPos = 0;
+                        for (endPos = startPos; endPos < readLen; endPos++)
+                            // SEPARATOR 발견
+                            if (buffer[endPos] == SEPARATOR)
+                            {
+                                // SEPARATOR 발견 전까지를 전부 가져오기
+                                content.Append(buffer, startPos, endPos - startPos);
+
+                                // 한 곡으로 생성
+                                for (titleEndPosition = 0; (titleEndPosition < content.Length) && (content[titleEndPosition] != CR); titleEndPosition++);
+                                if (titleEndPosition == content.Length)
+                                    lyricArray.Add(
+                                        new SingleLyric()
+                                        {
+                                            title = content.ToString(0, titleEndPosition),
+                                            content = ""
+                                        });
+                                else
+                                    lyricArray.Add(
+                                        new SingleLyric()
+                                        {
+                                            title = content.ToString(0, titleEndPosition),
+                                            content = content.ToString(titleEndPosition + 2, content.Length - (titleEndPosition + 2))
+                                        });
+
+                                content = new StringBuilder("");
+                                // SEPARATOR 뒤 공백문자는 모두 건너뜀
+                                for (startPos = endPos + 1; (startPos < readLen) && char.IsWhiteSpace(buffer[startPos]); startPos++);
+                                endPos = startPos - 1;
+                            }
+                        if (startPos < readLen)
+                            content.Append(buffer, startPos, endPos - startPos);
+                    }
+
+                    file.Close();
+                }
+            }
+            catch
+            {
+                lyricArray = null;
+            }
+
+            return lyricArray;
+        }
+
+        /// <summary>
+        /// 전체 가사를 파일로 저장합니다.
+        /// </summary>
+        /// <param name="lyricArray">
+        /// 전체 가사 리스트
+        /// </param>
+        public static void SaveAll(List<SingleLyric> lyricArray)
+        {
+            StreamWriter file = new StreamWriter(LYRIC_PATH,false);
+
+            try
+            {
+                foreach (SingleLyric lyric in lyricArray)
+                {
+                    file.WriteLine(lyric.title);
+                    file.Write(lyric.content);
+                    file.WriteLine(SEPARATOR);
+                }
+            }
+            catch (Exception e)
+            {
+                file.Close();
+                throw e;
+            }
+            file.Close();
+        }
+    }
+}
+
