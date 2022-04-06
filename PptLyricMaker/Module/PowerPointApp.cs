0c7f0abd019e1de1bef4e8bffd4074344aced292
patternplane <bss011202@naver.com>
2022-04-06 13:01:17 +0900

    [FIX] 파일 출력시 발생하는 에러 처리

--- PptLyricMaker/Module/PowerPointApp.cs
+++ PptLyricMaker/Module/PowerPointApp.cs
@@ -4,38 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Microsoft.Office.Interop.PowerPoint;
+using mspp = Microsoft.Office.Interop.PowerPoint;
+using System.Windows;
 
 namespace PptLyricMaker.Module
 {
     class PowerPointApp
     {
-        private Application App;
+        private mspp.Application App;
         public PowerPointApp()
         {
-            App = new Application();
+            App = new mspp.Application();
         }
 
         public void SavePptFile(String FormFile, String OutputPath , String content, int lineNumber)
         {
-            Presentation form = App.Presentations.Open(FormFile, WithWindow: Microsoft.Office.Core.MsoTriState.msoFalse);
-            Presentation ppt = App.Presentations.Add(Microsoft.Office.Core.MsoTriState.msoFalse);
-            ppt.PageSetup.SlideOrientation = form.PageSetup.SlideOrientation;
-            ppt.PageSetup.SlideSize = form.PageSetup.SlideSize;
-            ppt.PageSetup.SlideHeight = form.PageSetup.SlideHeight;
-            ppt.PageSetup.SlideWidth = form.PageSetup.SlideWidth;
+            try
+            {
+                mspp.Presentation form = App.Presentations.Open(FormFile, WithWindow: Microsoft.Office.Core.MsoTriState.msoFalse);
+                mspp.Presentation ppt = App.Presentations.Add(Microsoft.Office.Core.MsoTriState.msoFalse);
 
-            makeSlides(form, ppt, content, lineNumber);
+                ppt.PageSetup.SlideOrientation = form.PageSetup.SlideOrientation;
+                ppt.PageSetup.SlideSize = form.PageSetup.SlideSize;
+                ppt.PageSetup.SlideHeight = form.PageSetup.SlideHeight;
+                ppt.PageSetup.SlideWidth = form.PageSetup.SlideWidth;
 
-            form.Close();
-            ppt.SaveAs(OutputPath);
-            ppt.Close();
+                makeSlides(form, ppt, content, lineNumber);
+
+                form.Close();
+                ppt.SaveAs(OutputPath);
+                ppt.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("1. 버튼을 너무 여러번 눌렀거나\n2. 출력하려는 위치가 관리자 권한이 필요한 위치일 수 있습니다.\n(다른 위치에 저장하거나, 관리자 권한으로 실행해주세요.)","파일 저장 오류",MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show("에러 정보 : \n" + e.Message, "error");
+            }
         }
 
         public void GeneratePpt(String FormFile, String content, int lineNumber)
         {
-            Presentation form = App.Presentations.Open(FormFile, WithWindow: Microsoft.Office.Core.MsoTriState.msoFalse);
-            Presentation ppt = App.Presentations.Add();
+            mspp.Presentation form = App.Presentations.Open(FormFile, WithWindow: Microsoft.Office.Core.MsoTriState.msoFalse);
+            mspp.Presentation ppt = App.Presentations.Add();
             ppt.PageSetup.SlideOrientation = form.PageSetup.SlideOrientation;
             ppt.PageSetup.SlideSize = form.PageSetup.SlideSize;
             ppt.PageSetup.SlideHeight = form.PageSetup.SlideHeight;
@@ -45,11 +55,11 @@ namespace PptLyricMaker.Module
             form.Close();
         }
 
-        private void makeSlides(Presentation form,Presentation ppt, String content, int lineNumber)
+        private void makeSlides(mspp.Presentation form, mspp.Presentation ppt, String content, int lineNumber)
         {
             String[] lines = content.Split(new[] { (char)0xd, (char)0xa },StringSplitOptions.RemoveEmptyEntries);
             int lastSlide = 1;
-            Slide CurrentSlide;
+            mspp.Slide CurrentSlide;
             StringBuilder lyric = new StringBuilder("");
             int currentLine = 0;
             foreach (String line in lines)
@@ -65,7 +75,7 @@ namespace PptLyricMaker.Module
                     CurrentSlide = ppt.Slides[lastSlide++];
 
                     // 모든 text가능 shape마다 작업
-                    foreach (Shape s in CurrentSlide.Shapes)
+                    foreach (mspp.Shape s in CurrentSlide.Shapes)
                         if (s.HasTextFrame != Microsoft.Office.Core.MsoTriState.msoFalse)
                             s.TextFrame.TextRange.Text = s.TextFrame.TextRange.Text.Replace("/가사", lyric.ToString());
 
@@ -84,7 +94,7 @@ namespace PptLyricMaker.Module
                 CurrentSlide = ppt.Slides[lastSlide++];
 
                 // 모든 text가능 shape마다 작업
-                foreach (Shape s in CurrentSlide.Shapes)
+                foreach (mspp.Shape s in CurrentSlide.Shapes)
                     if (s.HasTextFrame != Microsoft.Office.Core.MsoTriState.msoFalse)
                         s.TextFrame.TextRange.Text = s.TextFrame.TextRange.Text.Replace("/가사", lyric.ToString());
             }
